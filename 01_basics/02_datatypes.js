69bf644caa102910f251492875fe3570efc9585b
DailyBiteMeals <97652806+pddev7@users.noreply.github.com>
2023-09-25 01:12:03 +0530

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+const name_student = "Prachi Deokate"
+let Number = 7
+let Boolean = true
+let bigNumber = 12345678901234567890n
+let myNull = null;
+let myUndefined = undefined;
+let mySymbol = Symbol( "mySymbol" );
+let myObject = { Prachi }
+
+console.log( name_student );
+
+console.log( typeof name_student );
