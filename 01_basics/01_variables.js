3a385330833c234153e26c9e61bc00883d37e397
DailyBiteMeals <97652806+pddev7@users.noreply.github.com>
2023-09-25 00:26:28 +0530

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const account_Id = "19141112"
+let balance = 1000
+// var name = "Prachi Deokate"
+Whatever = "not a good practice"
+
+console.log( account_Id );
+
+console.table( [ account_Id, balance, Whatever ] );
